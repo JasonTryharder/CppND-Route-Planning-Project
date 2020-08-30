e19fc084708cc526f2c1ae2d7027da0b8485b823
Jasontryharder <jasonsy88@outlook.com>
2020-08-29 22:55:39 -0400

    using git rm --cached foldername -f to remove cache and add again ( remove ./git in sub folder first)

---
+++ CppND-Route-Planning-Project/src/main.cpp
@@ -0,0 +1,99 @@
+#include <optional>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <io2d.h>
+#include "route_model.h"
+#include "render.h"
+#include "route_planner.h"
+
+using namespace std::experimental;
+
+static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
+{   
+    std::ifstream is{path, std::ios::binary | std::ios::ate};
+    if( !is )
+        return std::nullopt;
+    
+    auto size = is.tellg();
+    std::vector<std::byte> contents(size);    
+    
+    is.seekg(0);
+    is.read((char*)contents.data(), size);
+
+    if( contents.empty() )
+        return std::nullopt;
+    return std::move(contents);
+}
+
+int main(int argc, const char **argv)
+{    
+    std::cout << " TEST \n";
+    std::string osm_data_file = "";
+    if( argc > 1 ) {
+        for( int i = 1; i < argc; ++i )
+            if( std::string_view{argv[i]} == "-f" && ++i < argc )
+                osm_data_file = argv[i];
+    }
+    else {
+        std::cout << "To specify a map file use the following format: " << std::endl;
+        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+        osm_data_file = "../map.osm";
+    }
+    
+    std::vector<std::byte> osm_data;
+ 
+    if( osm_data.empty() && !osm_data_file.empty() ) {
+        std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
+        auto data = ReadFile(osm_data_file);
+        if( !data )
+            std::cout << "Failed to read." << std::endl;
+        else
+            osm_data = std::move(*data);
+    }
+    
+    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+    // user input for these values using std::cin. Pass the user input to the
+    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    // Build Model.
+    float start_x ,start_y ,end_x ,end_y ;
+    std::vector<float> v_float ;
+    std::string input_str;
+    std::cout<< "Please input start/finish point as start_x,start_y,end_x,end_y ";
+    getline(std::cin,input_str);
+    std::stringstream ss(input_str);
+    float i;
+    while (ss>>i)
+    {
+        v_float.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+    start_x = v_float.at(0);
+    start_y = v_float.at(1);
+    end_x   = v_float.at(2);
+    end_y   = v_float.at(3);
+    std::cout<< start_x << " X "<< start_y << " Y "<< end_x << " X "<< end_y << " Y\n ";
+    RouteModel model{osm_data};
+
+    // Create RoutePlanner object and perform A* search.
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
+    route_planner.AStarSearch();
+
+    std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+
+    // Render results of search.
+    Render render{model};
+
+    auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
+    display.size_change_callback([](io2d::output_surface& surface){
+        surface.dimensions(surface.display_dimensions());
+    });
+    display.draw_callback([&](io2d::output_surface& surface){
+        render.Display(surface);
+    });
+    display.begin_show();
+}
